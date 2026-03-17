9d270a61b3f34715547ba6c6a984c844eb2c7139
Aarthi S <aarthiii333@gmail.com>
2026-03-17 22:35:54 +0530

    Add twoSum function for solving Two Sum problem
    Implement Two Sum function using hashing.

---
+++ hashing/two_sums.py
@@ -0,0 +1,23 @@
+"""
+LeetCode: Two Sum
+Pattern: Hashing
+"""
+
+def twoSum(nums, target):
+    hashmap = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+
+        if complement in hashmap:
+            return [hashmap[complement], i]
+
+        hashmap[num] = i
+
+    return []
+
+
+# Example
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))
